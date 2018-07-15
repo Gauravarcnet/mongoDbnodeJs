42153219987db7197d4ed1029a96c7bcb2989f62
gaurav <gauravbansal0000@gmail.com>
2018-07-16 00:52:31 +0530

    crud operation of mongodb on filebasis encapusalation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const MongoClient=require('mongodb').MongoClient;
 const assert=require('assert');
+const dboper=require('./operations');
+
 
 const url='mongodb://localhost:27017/conFusion';
 
@@ -7,29 +9,51 @@ MongoClient.connect(url,{ useNewUrlParser: true },(err,client)=>{
   assert.equal(err,null);
 
   console.log("Connected correctly to server");
-
   const db = client.db("conFusion");
-  const collection=db.collection('dishes');
-  collection.insertOne({"name":"dosa","description":"healthy dish"},
-  (err,result)=>{
-    assert.equal(err,null);
+   const collection=db.collection('dishes');
+  // collection.insertOne({"name":"dosa","description":"healthy dish"},
+  // (err,result)=>{
+  //   assert.equal(err,null);
+  //
+  //   console.log("After insert then:\n");
+  //   console.log(result.ops);
+  //
+  //
+  //   collection.find({}).toArray((err,docs)=>{
+  //     assert.equal(err,null);
+  //
+  //     console.log("Find :\n");
+  //     console.log(docs);
+  //
+  //     collection.drop("dishes",(err,result)=>{
+  //       assert.equal(err,null);
+  //
+  //       client.close();
+  //
+  //     });
+  //   });
+  // });
+    dboper.insertDocument(db,{name:"Vadount",description:"test"},"dishes",(result)=>{
+      console.log("insert document:/n",result.ops);
 
-    console.log("After insert then:\n");
-    console.log(result.ops);
+      dboper.findDocuments(db,"dishes",(docs)=>{
+        console.log("found documents:/n",docs);
 
+        dboper.updateDocument(db,{name:"Vadount"},{description:"updated test"},"dishes",(result)=>{
+          console.log("Updated documents:/n",result.result);
 
-    collection.find({}).toArray((err,docs)=>{
-      assert.equal(err,null);
+          dboper.findDocuments(db,"dishes",(docs)=>{
+            console.log("found documents:/n",docs);
+            collection.drop("dishes",(result)=>{
+              console.log("Dropped collection");
+              client.close();
 
-      console.log("Find :\n");
-      console.log(docs);
+            });
 
-      collection.drop("dishes",(err,result)=>{
-        assert.equal(err,null);
-
-        client.close();
-
+        });
       });
     });
   });
+
+
 });
